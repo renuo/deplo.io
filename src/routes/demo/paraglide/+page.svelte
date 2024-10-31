a1bd4d0db686300327d82106b12682464b5edcef
Philip Breuer <philip.breuer@pm.me>
2024-10-31 18:07:57 +0100

    Move language switch

--- src/routes/demo/paraglide/+page.svelte
+++ src/routes/demo/paraglide/+page.svelte
@@ -1,15 +1,6 @@
 <script lang="ts">
-  import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
-  import { i18n } from '$lib/i18n';
-  import { page } from '$app/stores';
-  import { goto } from '$app/navigation';
+  import { switchToLanguage } from '$lib/i18n';
   import * as m from '$lib/paraglide/messages';
-
-  function switchToLanguage(newLanguage: AvailableLanguageTag) {
-    const canonicalPath = i18n.route($page.url.pathname);
-    const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
-    goto(localisedPath);
-  }
 </script>
 
 <h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
