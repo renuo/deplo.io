c1b8221ccb514a1a538a85e29b0a5ec5fb9e9e8a
Philip Breuer <philip.breuer@pm.me>
2024-11-02 15:33:20 +0100

    Add carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
   import HeroSection from '$lib/components/HeroSection.svelte';
+  import SupportedFrameworks from '$lib/components/SupportedFrameworks.svelte';
 </script>
 
 <main>
   <HeroSection />
+
+  <section title="features">
+    <SupportedFrameworks />
+  </section>
+
+  <section title="notes" class="container">mountain</section>
+  <section title="comparison" class="container">table</section>
+  <section title="register" class="container">register</section>
 </main>
