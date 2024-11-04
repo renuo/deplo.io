ebb202911f67c458b7182da431ab36bf8d3ccc56
Philip Breuer <philip.breuer@pm.me>
2024-11-04 20:34:11 +0100

    Add comparison table

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,6 @@ export default ts.config(
   },
   {
     files: ['**/*.svelte'],
-
     languageOptions: {
       parserOptions: {
         parser: ts.parser,
@@ -28,6 +27,6 @@ export default ts.config(
     },
   },
   {
-    ignores: ['build/', '.svelte-kit/', 'dist/'],
+    ignores: ['build/', '.svelte-kit/', 'dist/', 'src/lib/paraglide'],
   },
 );
