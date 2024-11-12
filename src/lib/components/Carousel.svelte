43230f4a4c6a3b4220dc68933025f403f5893d83
Philip Breuer <philip.breuer@pm.me>
2024-11-12 09:33:32 +0100

    Add features section (#2)
    * Add hero section
    
    * Add carousel
    
    * Add features section
    
    * Finish features section

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import type { HTMLAttributes } from 'svelte/elements';
+  import { twMerge } from 'tailwind-merge';
+
+  interface CarouselProps extends HTMLAttributes<HTMLDivElement> {
+    speed?: number;
+    children: Snippet;
+  }
+
+  let { speed = 0.01, children, class: className, ...rest }: CarouselProps = $props();
+
+  let scrolling = $state(false);
+  let scrollable: HTMLDivElement | null = $state(null);
+  let animationFrame = 0;
+  let lastTime = 0;
+  let direction = 1;
+  let scrollLeft = 0;
+
+  function animate(time: number) {
+    if (!lastTime) lastTime = time;
+    const deltaTime = time - lastTime;
+    lastTime = time;
+
+    const distance = speed * deltaTime;
+    const hasReachedEnd = scrollable!.scrollLeft + distance >= scrollable!.scrollWidth - scrollable!.clientWidth;
+    const hasReachedStart = scrollable!.scrollLeft - distance <= 0;
+
+    if (!scrolling) {
+      scrollLeft += distance * direction;
+      scrollable!.scrollLeft = scrollLeft;
+    }
+
+    if ((direction === 1 && hasReachedEnd) || (direction === -1 && hasReachedStart)) {
+      direction = -direction;
+    }
+
+    animationFrame = requestAnimationFrame(animate);
+  }
+
+  function onscroll() {
+    scrolling = true;
+    scrollLeft = scrollable!.scrollLeft;
+  }
+
+  function onscrollend() {
+    scrolling = false;
+  }
+
+  $effect(() => {
+    animationFrame = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(animationFrame);
+  });
+</script>
+
+<div
+  {...rest}
+  bind:this={scrollable}
+  class={twMerge('no-scrollbar overflow-x-scroll', className)}
+  {onscroll}
+  {onscrollend}
+>
+  {@render children()}
+</div>
+
+<style>
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  .no-scrollbar::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for IE, Edge and Firefox */
+  .no-scrollbar {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
   import HeroSection from '$lib/components/HeroSection.svelte';
+  import FeaturesSection from '$lib/components/FeaturesSection.svelte';
 </script>
 
 <main>
   <HeroSection />
+  <FeaturesSection />
+
+  <section title="notes" class="container">mountain</section>
+  <section title="comparison" class="container">table</section>
+  <section title="register" class="container">register</section>
 </main>
