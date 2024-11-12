66e15429c6fe6d150b90e47cb115d099138602f3
Philip Breuer <philip.breuer@pm.me>
2024-11-12 09:58:43 +0100

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import mountainVector from '$lib/assets/backgrounds/mountain_vector.svg';
+</script>
+
+<div class="bg-background">
+  <img class="w-full pt-[320px]" src={mountainVector} alt="mountain vector background" />
+</div>
+
+<main class="-mt-[5vw] flex w-screen flex-col items-center justify-center bg-mountain py-32 text-deploio">
+  <h1 class="text-9xl font-semibold">{$page.status}</h1>
+  <p class="text-4xl uppercase">{$page.error?.message}</p>
+</main>
