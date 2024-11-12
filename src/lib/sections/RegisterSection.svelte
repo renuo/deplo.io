aee33846d7aa1d62d596aed447e234e1bb36e4ae
Philip Breuer <philip.breuer@pm.me>
2024-11-12 10:52:04 +0100

    Add meta data including gtm

--- src/lib/sections/RegisterSection.svelte
+++ src/lib/sections/RegisterSection.svelte
@@ -27,7 +27,7 @@
     <p class="mx-auto max-w-[600px]">{m.register_description()}</p>
 
     <form
-      class="relative space-y-2 h-20"
+      class="relative h-20 space-y-2"
       method="POST"
       action="https://script.google.com/macros/s/AKfycbwT1lwiqSW1clYKkuHFrjcXYyjpZD-mreglv8ZvKqP7e-HrTPE3YPPno6zEOtuKAysKbw/exec"
       use:enhance={submit}
