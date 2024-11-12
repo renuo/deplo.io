d41c5416dcec0990d44a88088cbe03e9f34bd657
Philip Breuer <philip.breuer@pm.me>
2024-11-12 16:49:13 +0100

    Add pricing page (#7)
    * Add hero section
    
    * Add carousel
    
    * Add features section
    
    * Finish features section
    
    * Add notes section
    
    * Add comparison table
    
    * Add better comparison logos
    
    * Add footer navigation
    
    * Split up footer
    
    * Add register section
    
    * Update register section styles
    
    * Make links appear hovered if it is current page
    
    * Update background helper
    
    * Allow button component to act as anchor element
    
    * Fix carousel error
    
    * Add missing links
    
    * Fix wave image blocking clicks
    
    * Fix register styles
    
    * Add pricing page
    
    * Add error page

---
+++ src/lib/sections/PricingSection.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Background from '$lib/components/Background.svelte';
+  import deploioVideo from '$lib/assets/backgrounds/deploio_video.mp4';
+  import * as m from '$lib/paraglide/messages';
+</script>
+
+<Background
+  video={deploioVideo}
+  class="h-[600px]"
+  gradient-class="bg-gradient-to-t from-background via-80% to-transparent"
+  wrapper-class="h-full"
+>
+  <section class="container flex h-full flex-col justify-end space-y-6 pb-16 text-deploio">
+    <h1 class="max-w-[500px] text-5xl text-[46px] font-medium uppercase leading-[50px]">{m.pricing_title()}</h1>
+    <p class="max-w-[500px] text-lg">{m.pricing_description()}</p>
+  </section>
+</Background>
+
+<div class="bg-background">
+  <div class="container !pb-32 !pt-0">
+    <iframe
+      width="100%"
+      height="787px"
+      title="pricing"
+      allow="clipboard-read; clipboard-write"
+      src="https://pricing-calculator-deploio.e1b591d.deploio.app"
+      class="rounded-xl"
+    ></iframe>
+  </div>
+</div>
---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import RegisterSection from '$lib/sections/RegisterSection.svelte';
+  import PricingSection from '$lib/sections/PricingSection.svelte';
+</script>
+
+<main>
+  <PricingSection />
+  <RegisterSection />
+</main>
