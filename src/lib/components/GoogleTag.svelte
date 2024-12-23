385176b6cab805855cda8fa98938a7a62170f6fb
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-23 15:37:46 +0100

    Update google tags

---
+++ src/lib/components/GoogleTag.svelte
@@ -0,0 +1,21 @@
+<script>
+    const googleTagId = import.meta.env.VITE_GOOGLE_TAG_ID;
+
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        const scriptTag = document.createElement('script');
+        scriptTag.async = true;
+        scriptTag.src = `https://www.googletagmanager.com/gtag/js?id=${googleTagId}`;
+        document.head.appendChild(scriptTag);
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag(...args) {
+            window.dataLayer.push(args);
+        }
+        gtag('js', new Date());
+        gtag('config', googleTagId);
+    });
+
+    alert(`Tag: ${googleTagId}`)
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+if (!process.env.VITE_GOOGLE_TAG_ID) {
+  console.warn("Google Tag Id not specified")
+}
+
 export default defineConfig({
   plugins: [
     sveltekit(),
