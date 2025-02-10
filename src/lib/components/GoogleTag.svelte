469c0376a48c69e00b8a43c8c442965c20e28839
Chris <76159444+hunchr@users.noreply.github.com>
2025-02-10 08:44:46 +0000

    Fix linter

--- src/lib/components/GoogleTag.svelte
+++ src/lib/components/GoogleTag.svelte
@@ -1,19 +1,19 @@
 <script>
-    const googleTagId = import.meta.env.VITE_GOOGLE_TAG_ID;
+  const googleTagId = import.meta.env.VITE_GOOGLE_TAG_ID;
 
-    import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
 
-    onMount(() => {
-        const scriptTag = document.createElement('script');
-        scriptTag.async = true;
-        scriptTag.src = `https://www.googletagmanager.com/gtag/js?id=${googleTagId}`;
-        document.head.appendChild(scriptTag);
+  onMount(() => {
+    const scriptTag = document.createElement('script');
+    scriptTag.async = true;
+    scriptTag.src = `https://www.googletagmanager.com/gtag/js?id=${googleTagId}`;
+    document.head.appendChild(scriptTag);
 
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-            window.dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', googleTagId);
-    });
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', googleTagId);
+  });
 </script>
