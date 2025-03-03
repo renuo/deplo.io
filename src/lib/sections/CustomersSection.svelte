7a68a6e1e294583f669c56fb97b04c705db45f45
Daniel Bengl <daniel.bengl@renuo.ch>
2025-03-03 16:40:07 +0100

    Add customer section

---
+++ src/lib/sections/CustomersSection.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import Carousel from '$lib/components/Carousel.svelte';
+  import * as m from '$lib/paraglide/messages';
+  import { appear } from '$lib';
+
+  import bdo from '$lib/assets/logos/bdo.svg';
+  import bienenSchweiz from '$lib/assets/logos/bienen_schweiz.svg';
+  import stadtZuerich from '$lib/assets/logos/stadt_zuerich.svg';
+  import swissIct from '$lib/assets/logos/swiss_ict.svg';
+  import renuo from '$lib/assets/logos/renuo_logo.webp';
+  import nine from '$lib/assets/logos/nine_logo.webp';
+</script>
+
+<section class="relative overflow-x-clip bg-deploio pb-6 pt-8">
+  <div class="container relative z-10">
+    <span class="mb-6 block text-left text-lg font-normal text-white">{m.customers_trust()}</span>
+    
+    <Carousel>
+      <div
+        class="pointer-events-none flex w-max select-none items-center gap-[10vw] lg:w-full lg:justify-between lg:gap-16"
+        use:appear={{ delay: 0 }}
+      >
+        <img class="h-34 w-auto object-contain brightness-0 invert" src={bdo} alt="BDO" />
+        <img class="h-34 w-auto object-contain brightness-0 invert" src={bienenSchweiz} alt="Bienen Schweiz" />
+        <img class="h-34 w-auto object-contain brightness-0 invert" src={stadtZuerich} alt="Stadt Zürich" />
+        <img class="h-34 w-auto object-contain brightness-0 invert" src={swissIct} alt="Swiss ICT" />
+        <img class="h-34 w-auto object-contain brightness-0 invert" src={renuo} alt="Renuo" />
+        <img class="h-34 w-auto object-contain brightness-0 invert" src={nine} alt="Nine" />
+      </div>
+    </Carousel>
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import HeroSection from '$lib/sections/HeroSection.svelte';
+  import CustomersSection from '$lib/sections/CustomersSection.svelte';
   import FeaturesSection from '$lib/sections/FeaturesSection.svelte';
   import NotesSection from '$lib/sections/NotesSection.svelte';
   import ComparisonSection from '$lib/sections/ComparisonSection.svelte';
@@ -14,6 +15,7 @@
 
 <main class="relative z-0">
   <HeroSection />
+  <CustomersSection />
   <FeaturesSection />
   <NotesSection />
   <ComparisonSection />
