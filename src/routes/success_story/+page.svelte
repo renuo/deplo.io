167f767a9f757fbb14695e09bae84a8af83ace2a
Daniel Bengl <daniel.bengl@renuo.ch>
2025-03-04 11:38:06 +0100

    Update success story page

---
+++ src/routes/success_story/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import SuccessStoryHeroSection from '$lib/sections/SuccessStoryHeroSection.svelte';
+  import CustomersSection from '$lib/sections/CustomersSection.svelte';
+  import FeaturesSection from '$lib/sections/FeaturesSection.svelte';
+  import NotesSection from '$lib/sections/NotesSection.svelte';
+  import ComparisonSection from '$lib/sections/ComparisonSection.svelte';
+  import RegisterSection from '$lib/sections/RegisterSection.svelte';
+  import GoogleTag from '$lib/components/GoogleTag.svelte';
+</script>
+
+<svelte:head>
+  <title>Deploio - Schweizer PaaS</title>
+  <GoogleTag />
+</svelte:head>
+
+<main class="relative z-0">
+  <SuccessStoryHeroSection />
+  <NotesSection />
+  <RegisterSection />
+</main>
