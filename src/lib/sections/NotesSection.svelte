d5ce9e50e90077c06363f41b8a93e70036edee3f
Daniel Bengl <daniel.bengl@renuo.ch>
2025-03-04 11:22:56 +0100

    Update card component

--- src/lib/sections/NotesSection.svelte
+++ src/lib/sections/NotesSection.svelte
@@ -11,8 +11,7 @@
   import successStoryBackground from '$lib/assets/backgrounds/success_story_background.png'
   import mountainOne from '$lib/assets/backgrounds/mountain_1.webp';
   import swissMadeSoftware from '$lib/assets/swiss_made_software_and_hosting.webp';
-    import Link from '$lib/components/Link.svelte';
-    import Background from '$lib/components/Background.svelte';
+  import Link from '$lib/components/Link.svelte';
 </script>
 
 <img src={mountainVector} alt="mountain vector background" class="pointer-events-none -my-px w-full bg-deploio" />
@@ -20,33 +19,36 @@
 <section class="bg-mountain">
   <div class="container p-0">
     <div class="bg-white group relative z-10 text-deploio w-full border-t-2 border-deploio flex" style="box-shadow: 0px 30px 40px 0px #141D501A;">
-      <div class="md:w-1/2 pt-7 pb-7 pl-6 md:pl-6 md:pr-4 z-10">
+      <div class="md:w-1/2 pt-7 pb-7 pl-6 md:pl-6 md:pr-4 z-10 max-sm:mt-32">
         <h3 class="">{m.success_story_subtitle()}</h3>
-        <h2 class="text-h2 mb-5 mt-0 md:mt-0" use:appear={{ delay: 50 }}>{m.success_story_title()}</h2>
+        <h2 class="text-h2 mb-5 mt-0 md:mt-0 max-sm:font-normal" use:appear={{ delay: 50 }}>{m.success_story_title()}</h2>
         <p class="mb-8 text-[20px]" use:appear={{ delay: 100 }}>{m.success_story_description()}</p>
         <div class="flex" use:appear={{ delay: 150 }}>
           <Link
           href="#"
-          class="text-deploio"
+          class="text-deploio font-medium"
           target="_blank"
           >
             <div class="flex">
               {m.feature_why_button()}
-              <img src="{chevronIcon}">
+              <img src="{chevronIcon}" alt="chevron">
             </div>
           </Link>
         </div>
       </div>
-      <div class="w-full sm:w-1/2">
-        <div class="absolute w-full md:w-7/12 lg:h-full lg:right-0">
-          <img src="{successStoryBackground}" class="w-full h-full object-cover object-top">
+      <div class="w-0 sm:w-1/2">
+        <div class="absolute w-full md:w-7/12 lg:h-full max-sm:left-0 lg:right-0">
+          <img src="{successStoryBackground}" alt="Renuo success story" class="w-full h-full object-cover object-top">
         </div>
-        <div class="absolute w-full md:w-7/12 lg:h-full lg:right-0" style="
+        <div class="absolute w-full sm:hidden" style="background: linear-gradient(0deg, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)),
+linear-gradient(0deg, rgba(252, 250, 250, 0) 43.77%, rgba(252, 250, 250, 0.6) 79.97%),
+linear-gradient(0deg, rgba(20, 29, 80, 0.2), rgba(20, 29, 80, 0.2));"></div>
+        <div class="absolute w-full md:w-7/12 lg:h-full max-sm:left-0 lg:right-0" style="
   background: linear-gradient(0deg, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)),
   linear-gradient(90deg, rgba(252, 250, 250, 0) 48.71%, rgba(252, 250, 250, 0.6) 86.14%),
   linear-gradient(0deg, rgba(20, 29, 80, 0.2), rgba(20, 29, 80, 0.2));
         "></div>
-        <div class="absolute w-full md:w-7/12 lg:h-full lg:right-0" style="background: linear-gradient(90deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%);"></div>
+        <div class="absolute w-full md:w-7/12 lg:h-full max-sm:left-0 lg:right-0" style="background: linear-gradient(90deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%);"></div>
       </div>
     </div>
   </div>
