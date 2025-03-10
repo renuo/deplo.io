f04492b33d1618dc239ad152ea6962388d4f0e28
Daniel Bengl <daniel.bengl@renuo.ch>
2025-03-10 13:57:28 +0100

    Update breakpoints

--- src/lib/sections/SuccessStorySlackSection.svelte
+++ src/lib/sections/SuccessStorySlackSection.svelte
@@ -15,8 +15,8 @@
     class="pointer-events-none absolute left-0 right-0 top-0 z-0 w-full -rotate-6 scale-[3] opacity-5 sm:scale-[2] md:scale-[1.25]"
   />
 
-  <div class="mb-30 container relative z-10 flex gap-12">
-    <div class="flex w-1/2 flex-col gap-5">
+  <div class="mb-30 container relative z-10 flex flex-col lg:flex-row gap-12">
+    <div class="flex lg:w-1/2 flex-col gap-5">
       <SlackMessage
         baseDelay={200}
         name={m.success_story_slack_message_1_name()}
@@ -35,7 +35,7 @@
         {@html m.success_story_slack_message_2_text().replace('nine.ch', '<a href="https://nine.ch" class="text-blue-600">nine.ch</a>')}
       </SlackMessage>
     </div>
-    <div class="w-1/2">
+    <div class="lg:w-1/2">
       <div class="text-white" use:appear={{ delay: 100 }}>
         <h3 class="text-h3 pb-4">
           {m.success_story_slack_before_cloud()}
@@ -45,7 +45,7 @@
         </p>
       </div>
 
-      <div class="ml-auto w-2/3 text-white">
+      <div class="ml-auto xl:w-2/3 text-white">
         <h3 class="text-h3 mt-8 pb-4" use:appear={{ delay: 300 }}>{m.success_story_slack_beauty_beast()}</h3>
         <p use:appear={{ delay: 400 }}>
           {m.success_story_slack_beauty_beast_text()}
