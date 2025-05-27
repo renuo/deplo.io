99dc9d2e240e9532c2bfbbfcc3493a0b7dcc701a
Samuel Steiner <samuel.steiner@renuo.ch>
2025-05-27 14:36:44 +0200

    Correct Comparison

--- src/lib/sections/ComparisonSection.svelte
+++ src/lib/sections/ComparisonSection.svelte
@@ -32,9 +32,9 @@
         [true, false, true, false],
         [true, false, false, true],
         [true, true, false, false],
-        [true, false, true, false],
-        [true, false, false, true],
-        [false, true, false, false],
+        [true, true, true, true],
+        [true, false, true, true],
+        [false, true, true, false],
       ]}
     />
   </div>
