4c020d1b3bd648725884fbf1595af7e3c126a9b8
Etienne Kyburz <etienne.kyburz@renuo.ch>
2025-06-30 10:45:37 +0200

    Refactoring afeter review

--- src/lib/components/MemberCard.svelte
+++ src/lib/components/MemberCard.svelte
@@ -5,14 +5,14 @@
 </script>
 
 <div
-  class="flex h-[305px] w-[281.75px] flex-col items-stretch border-t-2 border-deploio bg-background shadow-md transition-transform duration-200 ease-in-out hover:-translate-y-2 hover:shadow-xl"
+  class="flex h-[305px] w-[281.75px] flex-col items-stretch border-t-2 border-deploio bg-background shadow-md duration-300 hover:-translate-y-2 hover:shadow-xl"
 >
   <div class="flex h-[93px] w-full flex-col items-start justify-start ps-6 pt-5">
     <div class="text-h5 text-deploio">{name}</div>
-    <div class="text-p mt-1 text-deploio">{role}</div>
+    <div class="text-p mt-3 text-deploio">{role}</div>
   </div>
   <div class="relative flex w-full flex-1 items-end justify-center overflow-hidden">
     <img src={image} alt={name} class=" z-10 h-[200px] object-cover" />
-    <img src="/src/lib/assets/backgrounds/mountain_vector.svg" alt="mountain vector" class="absolute z-0 w-full" />
+    <img src={`/src/lib/assets/backgrounds/team_mountain_vector_${Math.floor(Math.random() * 3) + 1}.svg`} alt="mountain vector" class="absolute z-0 w-full" />
   </div>
 </div>
---
+++ src/lib/data/team.ts
@@ -0,0 +1,39 @@
+import cyrillTroxler from '$lib/assets/team/cyrill_troxler.webp';
+import danielWilhelm from '$lib/assets/team/daniel_wilhelm.webp';
+import davorGajic from '$lib/assets/team/davor_gajic.webp';
+import etienneKyburz from '$lib/assets/team/etienne_kyburz.webp';
+import josuaSchmid from '$lib/assets/team/josua_schmid.webp';
+import kevinFerranti from '$lib/assets/team/kevin_ferranti.webp';
+import nikkyJolley from '$lib/assets/team/nikky_jolley.webp';
+import oliverAnthony from '$lib/assets/team/oliver_anthony.webp';
+import oliverNietlisbach from '$lib/assets/team/oliver_nietlisbach.webp';
+import pawelKuc from '$lib/assets/team/pawel_kuc.webp';
+import raphaelKnecht from '$lib/assets/team/raphael_knecht.webp';
+import raphaelNestler from '$lib/assets/team/raphael_nestler.webp';
+import samuelSteiner from '$lib/assets/team/samuel_steiner.webp';
+import timGrethler from '$lib/assets/team/tim_grethler.webp';
+import thomasHug from '$lib/assets/team/thomas_hug.webp';
+import vanessaAlbrecht from '$lib/assets/team/vanessa_albrecht.webp';
+import yessinBenBrahim from '$lib/assets/team/yessin_ben_brahim.webp';
+import sebastianNickel from '$lib/assets/team/sebastian_nickel.webp';
+
+export const team = [
+  { name: 'Thomas Hug', role: 'Co-Founder', image: thomasHug },
+  { name: 'Samuel Steiner', role: 'Co-Founder', image: samuelSteiner },
+  { name: 'Tim Grethler', role: 'Head of Engineering', image: timGrethler },
+  { name: 'Nikky Jolley', role: 'Customer Success', image: nikkyJolley },
+  { name: 'Josua Schmid', role: 'Strategie', image: josuaSchmid },
+  { name: 'Cyrill Troxler', role: 'Platform Engineer', image: cyrillTroxler },
+  { name: 'Sebastian Nickel', role: 'Platform Engineer', image: sebastianNickel },
+  { name: 'Davor Gajic', role: 'Platform Engineer', image: davorGajic },
+  { name: 'Pawel Kuc', role: 'Platform Engineer', image: pawelKuc },
+  { name: 'Daniel Wilhelm', role: 'Senior Engineer Managed Services', image: danielWilhelm },
+  { name: 'Raphael Nestler', role: 'Senior Software Engineer', image: raphaelNestler },
+  { name: 'Kevin Ferranti', role: 'Software Engineer', image: kevinFerranti },
+  { name: 'Oliver Nietlisbach', role: 'Software Engineer', image: oliverNietlisbach },
+  { name: 'Etienne Kyburz', role: 'Software Engineer', image: etienneKyburz },
+  { name: 'Raphael Knecht', role: 'Marketing', image: raphaelKnecht },
+  { name: 'Oliver Anthony', role: 'Dokumentation', image: oliverAnthony },
+  { name: 'Yessin Ben Brahim', role: 'Marketing', image: yessinBenBrahim },
+  { name: 'Vanessa Albrecht', role: 'Design', image: vanessaAlbrecht },
+];
