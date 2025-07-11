e5b98a4ac8012e8e3f98bb5967711e7019449ef1
Etienne Kyburz <etienne.kyburz@renuo.ch>
2025-07-11 08:55:23 +0200

    Fix bug and missing link team section

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -38,7 +38,7 @@
   });
 </script>
 
-<header class="container absolute left-0 right-0 top-0 z-20 flex items-center justify-between">
+<header class="container absolute left-0 right-0 top-0 z-30 flex items-center justify-between">
   <a
     href="/"
     class="flex select-none flex-row items-center justify-center gap-2 md:hidden {isOpen ? 'hidden' : ''}"
@@ -64,7 +64,7 @@
   </nav>
 
   <button 
-    class="md:hidden z-30 p-3 {isOpen ? 'fixed top-3 right-4' : 'relative'}"
+    class="md:hidden z-40 p-3 {isOpen ? 'fixed top-3 right-4' : 'relative'}"
     aria-label={isOpen ? 'Close menu' : 'Open menu'} 
     on:click={toggleMenu}
   >
@@ -79,7 +79,7 @@
 
 {#if isOpen}
   <div 
-    class="fixed inset-0 bg-white z-10 flex flex-col md:hidden pt-5"
+    class="fixed inset-0 bg-white z-20 flex flex-col md:hidden pt-5"
   >
     <div class="container">
       <a href="/" class="flex items-center">
@@ -111,6 +111,14 @@
         class="text-deploio py-3 text-2xl tracking-[0.02em] font-normal w-full"
       >
         {m.navigation_login()}
+      </a>
+
+      <a 
+        href="/team" 
+        on:click={toggleMenu}
+        class="text-deploio py-3 text-2xl tracking-[0.02em] font-normal w-full"
+      >
+        {m.navigation_team()}
       </a>
       
       <div class="h-8"></div>
