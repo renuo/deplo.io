d558f881a8fb2c915a836fe2e033cf3b673a2058
EtienneKyburz <114604643+ekyburz@users.noreply.github.com>
2025-07-14 13:46:44 +0200

    Improve team responsive design (#38)
    * Correct bg color
    
    * Fix bug and missing link team section
    
    * Remove unused color
    
    * Change card bg color

--- src/lib/components/MemberCard.svelte
+++ src/lib/components/MemberCard.svelte
@@ -12,14 +12,14 @@
 </script>
 
 <div
-  class="flex h-[305px] w-[281.75px] flex-col items-stretch border-t-2 border-deploio bg-background shadow-md duration-300 hover:-translate-y-2 hover:shadow-xl"
+  class="flex h-[305px] w-[281.75px] flex-col items-stretch border-t-2 border-deploio bg-team shadow-md duration-300 hover:-translate-y-2 hover:shadow-xl"
 >
   <div class="flex h-[93px] w-full flex-col items-start justify-start ps-6 pt-5">
     <div class="text-h5 text-deploio">{name}</div>
     <div class="text-p mt-3 text-deploio">{role}</div>
   </div>
   <div class="relative flex w-full flex-1 items-end justify-center overflow-hidden">
-    <img src={image} alt={name} class=" z-10 h-[200px] object-cover" />
+    <img src={image} alt={name} class="z-10 h-[200px] object-cover" />
     <img src={imageSrc} alt="mountain vector" class="absolute z-0 w-full" />
   </div>
 </div>
