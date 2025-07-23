58e9fba34209d1156295af8cb95acbfb02233bdb
EtienneKyburz <114604643+ekyburz@users.noreply.github.com>
2025-07-23 09:17:27 +0200

    Improve responsive design (#39)
    * Improve responsive design
    
    * Move mountain to the left
    
    * Apply suggestions from code review
    
    Co-authored-by: Oliver Anthony <108479068+oliveranthony17@users.noreply.github.com>
    
    * Small changes after pr review
    
    ---------
    
    Co-authored-by: Oliver Anthony <108479068+oliveranthony17@users.noreply.github.com>

--- src/lib/components/MountainSvg.svelte
+++ src/lib/components/MountainSvg.svelte
@@ -1,10 +1,15 @@
+<script lang="ts">
+  export let labels: string[] = [];
+  const [label1, label2, label3, label4] = labels;
+</script>
+
 <svg
   width="1440"
-  height="547"
-  viewBox="0 0 1440 547"
+  height="747"
+  viewBox="40 -200 1440 747"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
-  class="pointer-events-none -mb-px h-auto w-full select-none"
+  class="mountain-svg pointer-events-none -mb-px h-auto w-full select-none"
 >
   <path
     d="M1304.5 164L1455 303.5L1485.5 546.499H-68.0005L33 498.5L130 474.5L183 440L210.5 425L244.5 440L349 386L413 323.5H454L500.5 303.5L563 336.5L665 293L748 243L767 253L824 295.5L856 270.5L953.2 153.4L1089.5 114L1124 79.5L1150.5 66.5L1201 20L1232.5 46.5L1304.5 164Z"
@@ -14,7 +19,13 @@
   <circle cx="499" cy="303" r="18" stroke="#141D50" stroke-width="4" />
   <circle cx="747" cy="243" r="18" stroke="#141D50" stroke-width="4" />
   <circle cx="1200" cy="20" r="18" stroke="#141D50" stroke-width="4" />
-  <g clip-path="url(#clip0_2455_4589)">
+  
+  <text x="209" y="380" text-anchor="middle" class="mountain-label" fill="#141D50" transform="rotate(-30, 209, 380)">{label1}</text>
+  <text x="499" y="260" text-anchor="middle" class="mountain-label" fill="#141D50" transform="rotate(-30, 499, 260)">{label2}</text>
+  <text x="747" y="200" text-anchor="middle" class="mountain-label" fill="#141D50" transform="rotate(-30, 747, 200)">{label3}</text>
+  <text x="1200" y="-20" text-anchor="middle" class="mountain-label" fill="#141D50" transform="rotate(-30, 1200, -20)">{label4}</text>
+  
+  <g>
     <mask
       id="mask0_2455_4589"
       style="mask-type:alpha"
@@ -66,9 +77,6 @@
     </g>
   </g>
   <defs>
-    <clipPath id="clip0_2455_4589">
-      <rect width="986" height="433" fill="white" transform="translate(222 38)" />
-    </clipPath>
     <mask id="pulseMask">
       <rect width="1440" height="547" fill="black" />
       <circle r="140" fill="white">
@@ -98,5 +106,32 @@
     stroke-linecap: round;
     mask: url(#pulseMask);
     stroke-dashoffset: 0;
+  }
+  
+  .mountain-label {
+    font-family: inherit;
+    font-size: 45px;
+    font-weight: normal;
+    text-transform: uppercase;
+    line-height: 117%;
+    letter-spacing: 0.02em;
+  }
+
+  @media (min-width: 750px) {
+    .mountain-label {
+      font-size: 32px;
+    }
+  }
+  
+  @media (min-width: 1000px) {
+    .mountain-label {
+      font-size: 20px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .mountain-label {
+      font-size: 16px;
+    }
   }
 </style>
