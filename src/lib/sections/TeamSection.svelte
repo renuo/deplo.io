ba6d233bde62d6b36737a395fe63dc15abc7fc58
Julian Duss <julian.duss@renuo.ch>
2025-07-25 14:28:38 +0200

    Translate Team Text (#41)

--- src/lib/sections/TeamSection.svelte
+++ src/lib/sections/TeamSection.svelte
@@ -4,7 +4,22 @@
   import * as m from '$lib/paraglide/messages';
   import { appear } from '$lib';
   import MemberCard from '$lib/components/MemberCard.svelte';
+  import { page } from "$app/state";
+  import { i18n } from '$lib/i18n';
+  import { onMount } from "svelte";
   import { team } from '$lib/data/team';
+  import { team_en } from '$lib/data/team';
+
+  const language = i18n.getLanguageFromUrl(page.url)
+
+  let members: Array = [];
+  onMount(async () => {
+    if (language == "de") {
+      members = team;
+    } else {
+      members = team_en;
+    }
+  });
 </script>
 
 <Background
@@ -27,7 +42,7 @@
   <div class="team-container !pb-32 !py-2 my-0 !pt-16 md:!pt-40">
     <section>
       <div class="grid grid-cols-1 gap-8 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
-        {#each team as member}
+        {#each members as member}
           <MemberCard {...member} />
         {/each}
       </div>
