b2df1c5771f057d93ae406dc243fea4b9f639fc0
Samuel Steiner <samuel.steiner@renuo.ch>
2026-01-13 08:17:05 +0100

    Hyperlink certificate logos

--- src/lib/components/FooterCertificates.svelte
+++ src/lib/components/FooterCertificates.svelte
@@ -6,8 +6,26 @@
 
 <div class="container !mt-0">
   <div class="flex w-fit flex-wrap justify-center gap-8">
-    <img class="h-20" src={iso9001Icon} width="80" height="80" alt="ISO 9001:2015 certification" />
-    <img class="h-20" src={iso27001Icon} width="80" height="80" alt="ISO 27001:2022 certification" />
-    <img class="h-20" src={co2NeutralIcon} width="80" height="80" alt="CO2 neutral certification" />
+    <a
+      href="https://nine.ch/images/nine_ISO9001.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img class="h-20" src={iso9001Icon} width="80" height="80" alt="ISO 9001:2015 certification" />
+    </a>
+    <a
+      href="https://nine.ch/images/ISO-27001-Nine-Internet-Solutions-AG-2.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img class="h-20" src={iso27001Icon} width="80" height="80" alt="ISO 27001:2022 certification" />
+    </a>
+    <a
+      href="https://nine.ch/images/myclimate_confirmation_16836958-1.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img class="h-20" src={co2NeutralIcon} width="80" height="80" alt="CO2 neutral certification" />
+    </a>
   </div>
 </div>
