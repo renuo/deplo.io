3d0220d0b657b8a16b43b027ebd829d13a3ef660
Alessandro Rodi <coorasse@gmail.com>
2026-02-24 12:23:05 +0100

    Language switcher redesign (#46)
    * Add French and Italian
    
    * New language switcher

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -46,16 +46,13 @@
     <img class="h-7" src={deploioLogo} alt="deploio logo" />
     <img class="h-6" src={deploioName} alt="deploio" />
   </a>
-  
-  <a
-    href="/"
-    class="hidden lg:block absolute {isOpen ? 'opacity-0' : ''}"
-  >
+
+  <a href="/" class="hidden lg:block absolute {isOpen ? 'opacity-0' : ''}">
     <img class="w-48 h-48 object-contain mt-12" src={deploioLogoHeader} alt="deploio header logo" />
   </a>
 
   <nav class="hidden md:flex items-center gap-8 ml-auto -mt-10">
-    <LanguageSwitcher />
+    <LanguageSwitcher class="text-white" />
     <Link href="/pricing">{m.navigation_pricing()}</Link>
     <Link href="/team">{m.navigation_team()}</Link>
     <Link href="https://docs.deplo.io">{m.navigation_documentation()}</Link>
@@ -63,9 +60,9 @@
     <Button variant="primary" href="https://cockpit.nine.ch/{m.navigation_language()}/signup?origin=deploio" target="_blank">{m.navigation_start()}</Button>
   </nav>
 
-  <button 
+  <button
     class="md:hidden z-40 p-3 {isOpen ? 'fixed top-3 right-4' : 'relative'}"
-    aria-label={isOpen ? 'Close menu' : 'Open menu'} 
+    aria-label={isOpen ? 'Close menu' : 'Open menu'}
     on:click={toggleMenu}
   >
     {#if isOpen}
@@ -76,64 +73,63 @@
   </button>
 </header>
 
-
 {#if isOpen}
-  <div 
-    class="fixed inset-0 bg-white z-20 flex flex-col md:hidden pt-5"
-  >
+  <div class="fixed inset-0 bg-white z-20 flex flex-col md:hidden pt-5">
     <div class="container">
       <a href="/" class="flex items-center">
         <img class="h-7 -mt-1" src={deploioLogoMobile} alt="deploio logo" />
       </a>
     </div>
-    
-    <nav class="flex flex-col w-full pt-12 mt-4 container h-full relative">
-      <a 
-        href="/pricing" 
+
+    <nav class="flex flex-col w-full pt-12 mt-4 container h-full">
+      <a
+        href="/pricing"
         on:click={toggleMenu}
         class="text-deploio py-3 text-2xl tracking-[0.02em] font-normal w-full"
       >
         {m.navigation_pricing()}
       </a>
-      
-      <a 
-        href="https://docs.deplo.io" 
+
+      <a
+        href="https://docs.deplo.io"
         on:click={toggleMenu}
         class="text-deploio py-3 text-2xl tracking-[0.02em] font-normal w-full"
       >
         {m.navigation_documentation()}
       </a>
-      
-      <a 
-        href="https://cockpit.nine.ch/{m.navigation_language()}/session/new?origin=deploio" 
-        target="_blank" 
+
+      <a
+        href="https://cockpit.nine.ch/{m.navigation_language()}/session/new?origin=deploio"
+        target="_blank"
         on:click={toggleMenu}
         class="text-deploio py-3 text-2xl tracking-[0.02em] font-normal w-full"
       >
         {m.navigation_login()}
       </a>
 
-      <a 
-        href="/team" 
+      <a
+        href="/team"
         on:click={toggleMenu}
         class="text-deploio py-3 text-2xl tracking-[0.02em] font-normal w-full"
       >
         {m.navigation_team()}
       </a>
-      
-      <div class="h-8"></div>
-      
-      <a 
+
+      <div class="mt-auto"></div>
+
+      <LanguageSwitcher class="text-deploio" onSelect={toggleMenu} />
+
+      <div class="h-6"></div>
+
+      <a
         href="https://cockpit.nine.ch/{m.navigation_language()}/signup?origin=deploio"
         on:click={toggleMenu}
         class="bg-deploio text-white py-2 text-lg tracking-[0.02em] text-center w-full"
       >
         {m.navigation_start()}
       </a>
-      
-      <div class="absolute bottom-8 left-4">
-        <LanguageSwitcher />
-      </div>
+
+      <div class="h-8"></div>
     </nav>
   </div>
 {/if}
