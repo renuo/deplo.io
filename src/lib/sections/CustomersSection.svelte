68f06f2aab673864b1110805788ac245168378ac
Tim Landolt <timlandolt@gmx.ch>
2026-05-22 07:59:50 +0200

    Add logos
    # Conflicts:
    #	src/lib/assets/logos/demokratis.svg
    #	src/lib/sections/CustomersSection.svelte

--- src/lib/sections/CustomersSection.svelte
+++ src/lib/sections/CustomersSection.svelte
@@ -7,7 +7,31 @@
   import bienenSchweiz from '$lib/assets/logos/bienen_schweiz.svg';
   import stadtZuerich from '$lib/assets/logos/stadt_zuerich.svg';
   import swissIct from '$lib/assets/logos/swiss_ict.svg';
+  import modeWeber from '$lib/assets/logos/mode_weber.png';
   import demokratis from '$lib/assets/logos/demokratis.svg';
+  import swissstaffing from '$lib/assets/logos/swissstaffing.svg';
+  import gebaeudepass from '$lib/assets/logos/gebaeudepass.svg';
+  import brevalia from '$lib/assets/logos/brevalia.webp';
+  import tincan from '$lib/assets/logos/tincan.svg';
+  import ohoDesign from '$lib/assets/logos/oho_design.svg';
+  import jobRocks from '$lib/assets/logos/job_rocks.webp';
+  import kidesia from '$lib/assets/logos/kidesia.svg';
+
+  const customers = [
+    { src: stadtZuerich, alt: "Stadt Zürich" },
+    { src: bdo, alt: "BDO" },
+    { src: swissIct, alt: "Swiss ICT" },
+    { src: bienenSchweiz, alt: "Bienen Schweiz" },
+    { src: modeWeber, alt: "Modeweber", lazy: true },
+    { src: demokratis, alt: "Demokratis" },
+    { src: swissstaffing, alt: "swissstaffing", lazy: true },
+    { src: gebaeudepass, alt: "Gebäudepass ", lazy: true },
+    { src: brevalia, alt: "Brevalia", lazy: true },
+    { src: tincan, alt: "Tincan", lazy: true },
+    { src: ohoDesign, alt: "OHO Design", lazy: true },
+    { src: jobRocks, alt: "job.rocks", lazy: true },
+    { src: kidesia, alt: "Kidesia", lazy: true },
+  ]
 </script>
 
 <section class="relative overflow-x-clip bg-deploio pb-6 max-md:pt-10">
@@ -19,11 +43,12 @@
         class="pointer-events-none flex w-max select-none items-center gap-[10vw] lg:w-full lg:justify-between lg:gap-16"
         use:appear={{ delay: 0 }}
       >
-        <img class="h-34 w-auto object-contain brightness-0 invert" src={stadtZuerich} alt="Stadt Zürich" />
-        <img class="h-34 w-auto object-contain brightness-0 invert" src={bdo} alt="BDO" />
-        <img class="h-34 w-auto object-contain brightness-0 invert" src={swissIct} alt="Swiss ICT" />
-        <img class="h-34 w-auto object-contain brightness-0 invert" src={bienenSchweiz} alt="Bienen Schweiz" />
-        <img class="h-34 w-auto object-contain brightness-0 invert" src={demokratis} alt="Demokratis" />
+        {#each customers as customer }
+            <img class="h-34 w-auto object-contain brightness-0 invert"
+                 src={customer.src}
+                 alt={customer.alt}
+                 loading={customer.lazy ? 'lazy' : null} />
+        {/each}
       </div>
     </Carousel>
   </div>
