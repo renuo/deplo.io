31ac3e403c47e54f6a57f20c101ae2d8f15c28ae
Tim Landolt <111227353+timlandolt@users.noreply.github.com>
2026-06-03 16:14:20 +0200

    Replace mode weber with demokratis logo (#55)
    * Add demokratis logo
    
    * Remove modeweber logo
    
    * Shrink logo

--- src/lib/sections/CustomersSection.svelte
+++ src/lib/sections/CustomersSection.svelte
@@ -7,13 +7,13 @@
   import bienenSchweiz from '$lib/assets/logos/bienen_schweiz.svg';
   import stadtZuerich from '$lib/assets/logos/stadt_zuerich.svg';
   import swissIct from '$lib/assets/logos/swiss_ict.svg';
-  import modeWeber from '$lib/assets/logos/mode_weber.png';
+  import demokratis from '$lib/assets/logos/demokratis.svg';
 </script>
 
 <section class="relative overflow-x-clip bg-deploio pb-6 max-md:pt-10">
   <div class="container relative z-10">
     <span class="mb-6 block text-left text-lg font-normal text-white md:-mt-10">{m.customers_trust()}</span>
-    
+
     <Carousel>
       <div
         class="pointer-events-none flex w-max select-none items-center gap-[10vw] lg:w-full lg:justify-between lg:gap-16"
@@ -23,7 +23,7 @@
         <img class="h-34 w-auto object-contain brightness-0 invert" src={bdo} alt="BDO" />
         <img class="h-34 w-auto object-contain brightness-0 invert" src={swissIct} alt="Swiss ICT" />
         <img class="h-34 w-auto object-contain brightness-0 invert" src={bienenSchweiz} alt="Bienen Schweiz" />
-        <img class="h-34 w-auto object-contain brightness-0 invert" src={modeWeber} alt="Modeweber" loading="lazy" />
+        <img class="h-34 w-auto object-contain brightness-0 invert" src={demokratis} alt="Demokratis" />
       </div>
     </Carousel>
   </div>
