c3ccd8de1dde192620c1ff58bbdf4473dba73acd
Tim Landolt <timlandolt@gmx.ch>
2026-06-03 15:01:03 +0200

    Make notes scrollable

--- src/lib/sections/NotesSection.svelte
+++ src/lib/sections/NotesSection.svelte
@@ -60,7 +60,7 @@
     </div>
   </div>
 
-  <div class="relative z-10 mx-auto my-16 flex w-max gap-6 px-6 py-5 md:my-0 lg:justify-between">
+  <div class="relative z-10 mx-auto my-16 flex w-max no-scrollbar overflow-auto max-w-full gap-6 px-6 py-5 md:my-0 lg:justify-between">
     <Note title={m.notes_1_title()} description={m.notes_1_description()} />
     <Note title={m.notes_2_title()} description={m.notes_2_description()} />
     <Note title={m.notes_3_title()} description={m.notes_3_description()} />
