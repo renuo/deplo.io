36b59191a1bdd2975d1b9b23513aa5c1f5b480e1
Tim Landolt <timlandolt@gmx.ch>
2026-06-09 10:02:53 +0200

    Match slack section with design

--- src/lib/sections/SuccessStorySlackSection.svelte
+++ src/lib/sections/SuccessStorySlackSection.svelte
@@ -30,30 +30,28 @@
     class="pointer-events-none absolute left-0 right-0 top-0 z-0 w-full -rotate-6 scale-[3] opacity-5 sm:scale-[2] md:scale-[1.25]"
   />
 
-  <div class="mb-30 container relative z-10 flex flex-col gap-12 lg:flex-row">
-    <div class="flex flex-col gap-5 lg:w-1/2">
+  <div class="mb-30 container relative z-10 grid grid-cols-12 grid-rows-[auto_1fr]">
+    <div class="col-span-12 flex flex-col gap-5 lg:col-span-6 lg:row-span-2 lg:mr-9 xl:col-span-5">
       {#each messages as message}
         <SlackMessage baseDelay={message.baseDelay} name={message.name} avatar={message.avatar} date={message.date}>
           {@html message.content}
         </SlackMessage>
       {/each}
     </div>
-    <div class="lg:w-1/2">
-      <div class="text-white" use:appear={{ delay: 100 }}>
-        <h3 class="text-h3 pb-4">
-          {title1}
-        </h3>
-        <p use:appear={{ delay: 200 }}>
-          {@html text1}
-        </p>
-      </div>
 
-      <div class="ml-auto text-white xl:w-2/3">
-        <h3 class="text-h3 mt-8 pb-4" use:appear={{ delay: 300 }}>{title2}</h3>
-        <p use:appear={{ delay: 400 }}>
-          {text2}
-        </p>
-      </div>
+    <div class="col-span-12 mt-12 text-white lg:col-span-6 lg:mt-0" use:appear={{ delay: 100 }}>
+      <h3 class="text-h3 pb-4">
+        {title1}
+      </h3>
+      <p use:appear={{ delay: 200 }}>
+        {@html text1}
+      </p>
+    </div>
+    <div class="col-span-12 text-white lg:col-span-6 xl:col-span-5 xl:col-start-8">
+      <h3 class="text-h3 mt-8 pb-4" use:appear={{ delay: 300 }}>{title2}</h3>
+      <p use:appear={{ delay: 400 }}>
+        {text2}
+      </p>
     </div>
   </div>
 </section>
