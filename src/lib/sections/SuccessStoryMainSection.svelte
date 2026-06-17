4c537c9ca205cb4b0c36d519568c485ae7c2f8b8
Tim Landolt <timlandolt@gmx.ch>
2026-06-17 16:35:54 +0200

    Use quote instead of slack message

--- src/lib/sections/SuccessStoryMainSection.svelte
+++ src/lib/sections/SuccessStoryMainSection.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
+  import type { Snippet } from 'svelte';
   import { appear } from '$lib';
 
   import deploioWave from '$lib/assets/deploio_wave.svg';
-  import SlackMessage from '$lib/components/SlackMessage.svelte';
-
-  interface Message {
-    baseDelay: number;
-    avatar: string;
-    name: string;
-    date: string;
-    content: string;
-  }
 
   interface SuccessStorySlackSectionProps {
-    messages: Array<Message>;
     title1: string;
     text1: string;
     title2: string;
     text2: string;
+    children: Snippet;
   }
 
-  const { messages, title1, text1, title2, text2 }: SuccessStorySlackSectionProps = $props();
+  const { title1, text1, title2, text2, children }: SuccessStorySlackSectionProps = $props();
 </script>
 
 <section class="relative overflow-x-clip bg-deploio pb-16 pt-20">
@@ -32,12 +24,7 @@
 
   <div class="mb-30 container relative z-10 grid grid-cols-12 grid-rows-[auto_1fr]">
     <div class="col-span-12 flex flex-col gap-5 lg:col-span-6 lg:row-span-2 lg:mr-9 xl:col-span-5">
-      {#each messages as message}
-        <SlackMessage baseDelay={message.baseDelay} name={message.name} avatar={message.avatar} date={message.date}>
-          <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-          {@html message.content}
-        </SlackMessage>
-      {/each}
+      {@render children()}
     </div>
 
     <div class="col-span-12 mt-12 text-white lg:col-span-6 lg:mt-0" use:appear={{ delay: 100 }}>
--- src/routes/success_story/demokratis/+page.svelte
+++ src/routes/success_story/demokratis/+page.svelte
@@ -1,24 +1,15 @@
 <script lang="ts">
+  import { appear } from '$lib';
   import successStoryBackground from '$lib/assets/backgrounds/demokratis_success_story_background.webp';
-  import samuelAvatar from '$lib/assets/avatars/demokratis_fabian.webp';
+  import fabianAvatar from '$lib/assets/avatars/demokratis_fabian.webp';
   import SuccessStoryHeroSection from '$lib/sections/SuccessStoryHeroSection.svelte';
-  import SuccessStorySlackSection from '$lib/sections/SuccessStorySlackSection.svelte';
+  import SuccessStoryMainSection from '$lib/sections/SuccessStoryMainSection.svelte';
   import SuccessStoryNotesSection from '$lib/sections/SuccessStoryNotesSection.svelte';
   import RegisterSection from '$lib/sections/RegisterSection.svelte';
   import GoogleTag from '$lib/components/GoogleTag.svelte';
   import * as m from '$lib/paraglide/messages';
 
   const clientName = 'Demokratis';
-
-  const slackMessages = [
-    {
-      baseDelay: 200,
-      name: m.success_story_demokratis_slack_message_name(),
-      avatar: samuelAvatar,
-      date: m.success_story_demokratis_slack_message_date(),
-      content: m.success_story_demokratis_slack_message_text(),
-    },
-  ];
 </script>
 
 <svelte:head>
@@ -35,13 +26,36 @@
     clientWebsite="demokratis.ch"
     backgroundImage={successStoryBackground}
   />
-  <SuccessStorySlackSection
-    messages={slackMessages}
+  <SuccessStoryMainSection
     title1={m.success_story_demokratis_public_hand_title()}
     text1={m.success_story_demokratis_public_hand_text()}
     title2={m.success_story_demokratis_swissness_title()}
     text2={m.success_story_demokratis_swissness_text()}
-  />
+  >
+    <div
+      class="relative col-span-12 h-max border-t-2 border-deploio bg-white p-4 pb-10 font-medium text-deploio xl:col-span-4 xl:col-start-9"
+      style="box-shadow: 0px 30px 40px 0px #141D501A;"
+      use:appear={{ delay: 300 }}
+    >
+      <span class="text-2xl">
+        <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+        {@html m.success_story_demokratis_quote_text()}
+      </span>
+      <div class="mt-6 flex items-center font-normal">
+        <div class="border-t-1 mr-4 flex-shrink-0 border-deploio">
+          <img
+            src={fabianAvatar}
+            alt={m.success_story_demokratis_quote_name()}
+            class="m-2 h-full w-11 rounded-full bg-gray-200 object-cover"
+          />
+        </div>
+        <div class="text-base">
+          <p use:appear={{ delay: 350 }}>{m.success_story_demokratis_quote_name()}</p>
+          <p use:appear={{ delay: 400 }} class="opacity-70">{m.success_story_demokratis_quote_role()}</p>
+        </div>
+      </div>
+    </div>
+  </SuccessStoryMainSection>
   <SuccessStoryNotesSection
     title={m.success_story_demokratis_note_title()}
     text={m.success_story_demokratis_note_text()}
