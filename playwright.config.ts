1b21b51852a74705df3cea1a1c5d476d8af39100
Dani Bengl <53896675+cb341@users.noreply.github.com>
2026-09-08 15:55:40 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   webServer: {
     command: 'npm run build && npm run preview',
-    env: { PORT: '4173' },
     port: 4173,
   },
 
