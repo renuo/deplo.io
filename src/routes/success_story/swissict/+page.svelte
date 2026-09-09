2f106f8c3ffdb64c08b61deda66f7138fbebe97b
Jonathan Stalberg <jonathan.stalberg@renuo.ch>
2026-09-09 08:28:22 +0200

    Add swissICT success story page

---
+++ src/routes/success_story/swissict/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { appear } from '$lib';
+  import SuccessStoryHeroSection from '$lib/sections/SuccessStoryHeroSection.svelte';
+  import SuccessStoryMainSection from '$lib/sections/SuccessStoryMainSection.svelte';
+  import SuccessStoryNotesSection from '$lib/sections/SuccessStoryNotesSection.svelte';
+  import RegisterSection from '$lib/sections/RegisterSection.svelte';
+  import GoogleTag from '$lib/components/GoogleTag.svelte';
+  import * as m from '$lib/paraglide/messages';
+  import successStoryBackground from '$lib/assets/backgrounds/swissict_success_story_background.webp';
+  import corneliaAvatar from '$lib/assets/avatars/swissict_cornelia.webp';
+
+  const clientName = 'swissICT';
+</script>
+
+<svelte:head>
+  <title>{m.meta_title_success_story({ clientName })}</title>
+  <meta name="description" content={m.success_story_swissict_description()} />
+  <GoogleTag />
+</svelte:head>
+
+<main class="relative z-0">
+  <SuccessStoryHeroSection
+    title={m.success_story_swissict_title()}
+    description={m.success_story_swissict_long_description()}
+    {clientName}
+    clientWebsite="swissict.ch"
+    backgroundImage={successStoryBackground}
+  />
+  <SuccessStoryMainSection
+    title1={m.success_story_swissict_challenge_title()}
+    text1={m.success_story_swissict_challenge_text()}
+    title2={m.success_story_swissict_solution_title()}
+    text2={m.success_story_swissict_solution_text()}
+  >
+    <div
+      class="relative col-span-12 h-max border-t-2 border-deploio bg-white p-6 pb-14 font-medium text-deploio xl:col-span-4 xl:col-start-9"
+      style="box-shadow: 0px 30px 40px 0px #141D501A;"
+      use:appear={{ delay: 300 }}
+    >
+      <span class="text-2xl">
+        {m.success_story_swissict_quote_text()}
+      </span>
+      <div class="mt-6 flex items-center font-normal">
+        <div class="border-t-1 mr-4 flex-shrink-0 border-deploio">
+          <img
+            src={corneliaAvatar}
+            alt={m.success_story_swissict_quote_name()}
+            class="m-2 h-full w-11 rounded-full bg-gray-200 object-cover"
+          />
+        </div>
+        <div class="text-base">
+          <p use:appear={{ delay: 350 }}>{m.success_story_swissict_quote_name()}</p>
+          <p use:appear={{ delay: 400 }} class="opacity-70">{m.success_story_swissict_quote_role()}</p>
+        </div>
+      </div>
+    </div>
+  </SuccessStoryMainSection>
+  <SuccessStoryNotesSection title={m.success_story_swissict_note_title()} text={m.success_story_swissict_note_text()} />
+  <RegisterSection />
+</main>
